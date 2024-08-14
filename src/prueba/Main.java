c3a48d9637bb03cdf6e6cf9f21357c42ae52c1b8
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-08-13 19:39:38 -0600

    modify 1

--- src/prueba/Main.java
+++ src/prueba/Main.java
@@ -3,5 +3,6 @@ package prueba;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Prueba!");
+        System.out.println("Prueba desde modi 1!");
     }
 }
